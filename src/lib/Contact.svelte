af7c931443172333d8fa7bae3bc3c4b9d967ab97
Dennis Mwangi <rieratech@gmail.com>
2023-02-12 23:18:04 +0300

    started overhaul of chatbot

--- src/lib/Contact.svelte
+++ src/lib/Contact.svelte
@@ -1,11 +1,14 @@
 <script>
     import { afterUpdate, onMount } from "svelte"
-    import { PUBLIC_HOST } from "$env/static/public";
-    import { error } from "@sveltejs/kit";
+    import { PUBLIC_HOST } from "$env/static/public"
+    import { validateInput } from "$lib/js/validateInput";
 
     function mssgTime(){
         return new Date().toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })
     }
+    onMount(() => {
+        contactOffset = mssgBox.offsetTop - windowHeight
+    })
     /**
      * @type {number}
      */
@@ -16,20 +19,26 @@
     let windowHeight
     class Mssg{
         /**
-         * @param {string} message
+         * @param {string } message
          * @param {string | undefined} time
          * @param {string} author
+         * @param {string [] | undefined} buttons
          */
-        constructor(message, time, author){
+        constructor(message, time, author, buttons = []){
             this.message = message,
             this.time =  time || mssgTime(),
-            this.author = author;
+            this.author = author
+            this.buttons = buttons
         }
     }
     /**
-     * @type {Object.<string, string>}
+     * @type {HTMLInputElement}
      */
-    let form = {}
+    let submitBtn
+    /**
+     * @type {HTMLFormElement}
+     */
+    let form
     /**
      * @type {string}
      */
@@ -42,79 +51,14 @@
      * @type {number}
      */
     let contactOffset
-    let currentRequirement = 0
-    let listOfRequirements = [
-        {title: "name", message: "What's your name ?", error:"please enter a valid name"},
-        {title: "email", message: "what's your email address?", error:"please enter a valid email address"},
-        {title: "message", message: "leave us your message", error:"your message must be longer than a single character. thanks"}
-    ]
     /**
-     * @param {Mssg []} chatMessages
+     * @type {Mssg []}
      */
     let chatMessages = [new Mssg ("hi there", undefined,"bot")]
-    $: if(contactOffset < scroll - 80 && chatMessages.length < 1) chatMessages = [...chatMessages, new Mssg (listOfRequirements[0].message, undefined,"bot")]
-
-    async function scrollToBottomOfChat(){
-        mssgBox.scroll({ top: mssgBox.scrollHeight, behavior: 'smooth' });
-    }
     /**
-     * @param {string} title
-     * @param {string} inputMssg
+     * @type {{title: string; mssg: string; errorMssg: string; buttons: string [], next: any} []}
      */
-    function validateContactInput(title, inputMssg){
-        if(title==="name"){ 
-            return /^[A-Za-z][A-Za-z\'\-]+([\ A-Za-z][A-Za-z\'\-]+)*/.test(inputMssg)
-        }
-        if(title==="email") {
-            return /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/.test(inputMssg)
-        }
-        if(title==="message") {
-            return inputMssg.trim().length > 1
-        }
-    }
-    
-    async function sendMessage(){
-        if(inputMssg.trim() === "" || inputMssg.trim() === undefined || currentRequirement >= listOfRequirements.length) return
-        /**
-        * @param {string} title
-        */
-        let title = listOfRequirements[currentRequirement].title
-        form[title] = inputMssg
-        inputMssg = ""
-        chatMessages = [...chatMessages, new Mssg(form[title], undefined, "user")]
-
-        async function botReply(){
-            if( validateContactInput(title, form[title]) ){
-                currentRequirement += 1
-                if(currentRequirement >= listOfRequirements.length ){
-                    const reply = await fetch(`${PUBLIC_HOST}/contact`,{ 
-                        method: 'POST',
-                        body: JSON.stringify(form),
-                        headers:{
-                            'content-type': 'application/json'
-                        }
-                    })
-                    chatMessages = [...chatMessages, new Mssg(`Thank you, ${form.name}, We have received your message. Please keep checking your email for a reply from us`, undefined, "bot")]
-                }else{
-                    chatMessages = [...chatMessages, new Mssg(listOfRequirements[currentRequirement].message, undefined, "bot")]
-                }
-            }else{
-                chatMessages = [...chatMessages, new Mssg(listOfRequirements[currentRequirement].error, undefined, "bot")]
-            }
-        }
-
-    setTimeout(async() => await botReply(), 500)
-
-    }
-    onMount(() => {
-        contactOffset = mssgBox.offsetTop - windowHeight
-    })
-    afterUpdate(() => {
-        scrollToBottomOfChat()
-    })
-    let userMode
-    let botMode
-    let botModes = [
+    let modes = [
         {
             title: "name",
             mssg: "What is your full name?",
@@ -151,13 +95,6 @@
             next: ()=>"end",
         },
         {
-            title: "intro",
-            mssg: "What topics are you interested in?",
-            errorMssg: "please select a valid option",
-            buttons: ["leave us a message", "corporate training", "individual consultation", "speaking engagement"],
-            next: ()=>"name",
-        },
-        {
             title: "objectives",
             mssg: "What are your key objectives?",
             errorMssg: " ",
@@ -165,6 +102,53 @@
             next: ()=>"secondBranch()",
         }
     ]
+
+    let selections
+    let firstMessage = {
+        title: "intro",
+        mssg: "What topics are you interested in?",
+        errorMssg: "please select a valid option",
+        buttons: ["leave us a message", "corporate training", "individual consultation", "speaking engagement"],
+        next: ()=>"name",
+    }
+    let currentMode = firstMessage
+
+    $: if(contactOffset < scroll - ( windowHeight * 0.3 ) && chatMessages.length <= 1){
+        chatMessages = [...chatMessages, new Mssg (firstMessage.mssg, undefined,"bot",firstMessage.buttons)]
+    }
+    $: console.log(chatMessages)
+
+    async function scrollToBottomOfChat(){
+        mssgBox.scroll({ top: mssgBox.scrollHeight, behavior: 'smooth' });
+    }
+
+    async function sendMessage(){
+        chatMessages = [...chatMessages, new Mssg(inputMssg,undefined,"user")]
+        async function botReply(){
+            if( currentMode.buttons.length > 0){
+                if(!currentMode.buttons.includes(inputMssg)){
+                    chatMessages = [...chatMessages, new Mssg(currentMode.errorMssg,undefined,"bot")]
+                }else{
+                    selections.push({title: currentMode.title, selection: inputMssg})
+                }
+            }else{
+                //validate
+            }
+        }
+
+    setTimeout(async() => await botReply(), 500)
+
+    }
+    afterUpdate(() => {
+        scrollToBottomOfChat()
+    })
+    /**
+     * @param {string} button
+     */
+    function replyByButton(button){
+        inputMssg = button
+        submitBtn.click()
+    }
 </script>
 <style>
     .chatbox{
@@ -199,6 +183,23 @@
         display: grid;
         margin: 8px 0;
     }
+    .message_box > .selections{
+        animation-name: appear;
+        animation-duration: 500ms;
+        animation-fill-mode: both;
+        animation-timing-function: ease-out;
+        display: grid;
+        gap: 4px;
+        grid-template-columns: 1fr 1fr;
+        padding: 2px;
+        width: 50%;
+    }
+    .message_box > .selections > button{
+        background-color: rgba(25, 25, 25, 0.02);
+    }
+    .message_box > .selections > button:hover{
+        background-color: rgba(25, 25, 25, 0.05);
+    }
     .message > p{
         box-sizing: border-box;
         border-radius: var(--border_radius_secondary);
@@ -230,9 +231,6 @@
     .message.user > .time{
         text-align: right;
     }
-@media only screen and (max-width: 600px){
-
-}
 </style>
 <svelte:window bind:scrollY={scroll} bind:innerHeight={windowHeight}/>
 <section id="contact">
@@ -241,10 +239,18 @@
         <div class="message_box" bind:this={mssgBox}>
             {#each chatMessages as chatMessage}
                 <div class="message {chatMessage.author}"><p>{chatMessage.message}</p><span class="time">{chatMessage.time}</span></div>
+                {#if chatMessage.buttons.length > 0}
+                    <div class="selections">
+                        {#each chatMessage.buttons as button}
+                            <button on:click={()=>replyByButton(button)}>{ button }</button>
+                        {/each}
+                    </div>
+                {/if}
             {/each}
         </div>
-        <form on:submit|preventDefault={sendMessage}>
-            <input type="text" placeholder="write your {listOfRequirements[currentRequirement >= listOfRequirements.length ? listOfRequirements.length - 1 : currentRequirement].title} here, then press enter to send." bind:value={inputMssg}/>
+        <form on:submit|preventDefault={sendMessage} bind:this={form}>
+            <input type="text" placeholder="write your  here, then press enter to send." bind:value={inputMssg}/>
+            <input type="submit" style="display: none;" bind:this={submitBtn}/>
         </form>
     </div>
 </section>
